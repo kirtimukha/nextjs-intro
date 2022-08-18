f2551d12b9e4a87227ad4a66ccfbf2db6776cba8
kirti <kirtimukha@nate.com>
2022-08-19 07:50:51 +0900

    [U] router로 정보 전달하기

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import Layout from "../components/Layout";
 
 export default function App({Component, pageProps}) {
+    // pageProps 를 통해서 server side 를 통해 props를 페이지로 보낼 수 있다.
     return(
         <Layout>
             <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import Seo from "../components/Seo";
 import {useEffect, useState} from "react";
+import Link from "next/link";
+import {useRouter} from "next/router";
 
 //const API_KEY ="cdb8382d319ee2d3230abca55d55fe13";
 //api_key를 숨기기 위해 config.js로 옮김
 
-export default function Home() {
+export default function Home({results}) {
+    const router = useRouter();
+    const onClick = (id, title) => {
+      //  router.push(`/movies/${id}`)
+        router.push({
+            pathname:`/movies/${id}`,
+            query: {
+                title,
+            }
+    }, `/movies/${id}`)
+    }
     const [movies, setMovies ] = useState();
     useEffect(() => {
         (async() => {
@@ -16,7 +28,6 @@ export default function Home() {
             setMovies(results);
         })();
     }, []);
-
     return (
         <>
         <Seo title="Home"/>
@@ -24,9 +35,20 @@ export default function Home() {
             <Seo title="Home" />
             {!movies && <h4>Loading...</h4>}
             {movies?.map((movie) => (
-                <div className="movie" key={movie.id}>
+                <div className="movie" key={movie.id} onClick={() => onClick(movie.id,movie.original_title)}>
                     <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-                    <h4>{movie.original_title}</h4>
+                    <h4>
+                        <Link href={{
+                                pathname:`/movies/${movie.id}`,
+                                query: {
+                                    title:movie.original_title,
+                                },
+                            }}
+                              as={`/movies/${movie.id}`}
+                        >
+                            <a>{movie.original_title}</a>
+                        </Link>
+                    </h4>
                 </div>
             ))}
             <style jsx>{`
@@ -54,3 +76,27 @@ export default function Home() {
         </>
     );
 }
+
+
+
+/* Next JS는 해당 페이지가 서버 사이드 렌더링만 할 것인지를 선택할 수 있게 해준다. */
+/* 로딩 같은 것을 할 수도 있지만   */
+/* 서버에서 모두 렌더 하고 로딩 없이 보여줄 수도 있다 */
+/* 데이터가 들어옴과 동시에 렌더를 하는 것 */
+
+/*
+export async function getServerSideProps(){
+    // 서버에서만 작동하는 함수임
+    const {results} = await(
+        await fetch (`http://localhost:3000/api/movies`
+            //api_key를 숨기기 위해 config.js로 옮김
+        )
+    ).json();
+    return {
+        //서버에서 무엇을 리턴하든지, 그 내용을 props 키를 통해 home 페이지에 전달한다.
+        props:{
+           results,
+        }
+   }
+   // home component의 moveis를 results 로 변경해 주어야 한다.
+}*/
